4acf702563deb9d9289302a448c7c5360df0491d
Rainer J Fronz <rainerfronz44@gmail.com>
2020-09-09 02:24:17 -0500

    working.
    kind of

--- script.js
+++ script.js
@@ -1,54 +1,41 @@
-
+"use strict";
 
 // function to take username from form//
-
+function enterUser() {
+    let input = $("#user-input").val();
+    console.log("it worked");
+    return input;
+}
 // function for submit button//
 function submitButton() {
-    $('#search').submit(event => {
+    console.log('it works');
+    $("#search").submit(e => {
         e.preventDefault();
-        console.log('itworks')
-        fetchEnterUser(userId);
-    })
+        // const userID = $("#user-input").val();
+        getInfo();
 
+    });
 }
-function enterUser() {
-    let userId = $('#user-input').val();
-    console.log(enterUser);
-    return userId;
-}
-
-
 // function to make fetch to github//
-
-function getInfoFromGit() {
-
-    fetch('https://api.github.com/users/' + enterUser() + '/repos')
-    console.log('works')
-        .then(response => responsejson())
-    console.log(responsejson)
-        .then(responsejson => displayResults(responsejson))
-    console.log(responsejson)
-        .catch(error => alert('That User ID does not exist'));
-
-
+function getInfo() {
+    let url = `https://api.github.com/users/` + enterUser()+`/repos`
+    console.log(url);
+    fetch(url)
+        .then(response => response.Json())
+        .then(responseJson => console.log(responseJson))
+         .catch(error => alert('That User ID does not exist'));
+        enterUser();
 }
-
-
 // //handle and display result in DOM//
 // function displayResults(responseJson) {
 //     console.log(responseJson);
 //     $('#profile').empty();
 //     let responseHtml = "";
-
-
-
-//     }
-
+//  }
 // }
-
-// handle page and ready//
-$("document").ready(function () {
+// // handle page and ready//
+$(function () {
     console.log('lets git some');
     submitButton();
-
 });
+
